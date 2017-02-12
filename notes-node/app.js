696754e2f744cb36d78bf6059599e4ca11cb62c7
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-12 15:54:49 +0100

    Learn require file

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,14 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
+
+console.log('Result: ', notes.add(9, -2));
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
   if (err) {
     console.log('Unable to write to file');
   }
