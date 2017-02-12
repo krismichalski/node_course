8caac16acd6196be8ebed0394f8ed7079a7773c8
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-12 16:10:12 +0100

    Learn npm require

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,13 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+console.log(_.isString(true));
+console.log(_.isString('km'));
+var filteredArray = _.uniq(['km', 1, 'km', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 console.log('Result: ', notes.add(9, -2));
 
