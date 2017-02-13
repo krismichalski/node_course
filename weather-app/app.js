bdbb4512fd6ca2cf61dbb2cd298004a3a1d04cb3
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-13 16:09:37 +0100

    Learn pretty print

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=wyzwolenia%2018%20szczecin',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
