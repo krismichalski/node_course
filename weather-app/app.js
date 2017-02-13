441eeda51c7d9e97e20140a6220a7b783a360111
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-13 18:06:46 +0100

    Add requests to weather API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,39 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+//   .options({
+//     address: {
+//       describe: 'Address to fetch weather for',
+//       demand: true,
+//       alias: 'a',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode.js');
+var apiKey = '4fe5b68d1ee045ef5bff7fef478c2911';
 
-const argv = yargs
-  .options({
-    address: {
-      describe: 'Address to fetch weather for',
-      demand: true,
-      alias: 'a',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+const request = require('request');
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+request({
+  url: `https://api.darksky.net/forecast/${apiKey}/53.4285438,14.5528116`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(`Current temperature: ${body.currently.temperature}`);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
