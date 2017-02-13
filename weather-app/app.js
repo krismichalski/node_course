606e4d2bc90d60149e646c7f5fb74ef23986ad98
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-13 16:38:42 +0100

    Learn encodeURIComponent

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      describe: 'Address to fetch weather for',
+      demand: true,
+      alias: 'a',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=wyzwolenia%2018%20szczecin',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
