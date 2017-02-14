6694926d686aa881ba972f4270de083689c67de7
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-14 17:21:05 +0100

    Refactor tests to use assertion library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,41 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(8);
 
-  if (res !== 64) {
-    throw new Error(`Expected 64, but got ${res}.`);
-  }
+  expect(res).toBe(64).toBeA('number');
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11);
+//   // expect({name: 'Krzysztof'}).toEqual({name: 'Krzysztof'});
+//   // expect([2,3,4]).toInclude(3);
+//   expect({
+//     name: 'Krzysztof',
+//     age: 22,
+//     location: 'Szczecin'
+//   }).toInclude({
+//     age: 22
+//   });
+// });
+
+it('should verify first and last names are set', () => {
+  var user = {
+    age: 22,
+    location: 'Szczecin'
+  };
+  var res = utils.setName(user, 'Krzysztof Michalski');
+
+  expect(res).toBeA('object').toEqual(user).toInclude({
+    firstName: 'Krzysztof',
+    lastName: 'Michalski'
+  });
 });
