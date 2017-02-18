a3bf03a4c105b5eb7117f4d227c9cc3fceab740d
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-18 18:44:30 +0100

    Fetching data from MongoDB

---
+++ node-todo-api/playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Conneced to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58a876246e2c617ba4c16879')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Krzysztof'}).toArray().then((docs) => {
+    console.log('Users:');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  // db.close();
+});
