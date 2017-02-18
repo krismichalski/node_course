30dba435deac47feedf5ddfad1b35abde4363046
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-18 15:08:53 +0100

    Learn describe

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,27 +3,35 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+describe('Server', () => {
 
-it('should return my user', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Krzysztof',
-        age: 22
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('should return my user', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Krzysztof',
+            age: 22
+          });
+        })
+        .end(done);
+    });
+  });
+  
 });
