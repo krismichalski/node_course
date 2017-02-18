f3b6136acc1ebff6fb8ecb68d871617f36ef389d
Krzysztof Michalski <git@mail.nooulaif.com>
2017-02-18 14:23:05 +0100

    Learn async Mocha testing

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -8,10 +8,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('should square a number', () => {
   var res = utils.square(8);
 
   expect(res).toBe(64).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(3, (square) => {
+    expect(square).toBe(9).toBeA('number');
+    done();
+  });
 });
 
 // it('should expect some values', () => {
