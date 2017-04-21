fe405ee30cd3e045f646a6e6f0d1b188848983f9
Kristopher Michalski <nooulaif@users.noreply.github.com>
2017-04-21 19:35:30 +0200

    Add queries playground file

---
+++ node-todo-api/playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '58fa3e8576f8d943157dfc331';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos:', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo:', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo by ID:', todo);
+// }).catch((e) => console.log(e));
+
+var userID = '58fa142ee73ef3bb3524448c';
+
+User.findById(userID).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
